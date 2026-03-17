b90c58c69d8b163a1e6b3e620042f246b5f06689
programmist123 <magalimam.com@gmail.com>
2026-03-18 00:01:26 +0300

    v1.1
    исправление

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import subprocess
 
 way_app = []
 name_app = []
-
-print("▄▀▀░ ▄▀▄ █▄░▄█ █▀▀ ▄▀ █▀▀ █▄░█ ▀█▀ █▀▀ █▀▀▄ ")
-print("█░▀▌ █▀█ █░█░█ █▀▀ █░ █▀▀ █░▀█ ░█░ █▀▀ █▐█▀ ")
-print("▀▀▀░ ▀░▀ ▀░░░▀ ▀▀▀ ░▀ ▀▀▀ ▀░░▀ ░▀░ ▀▀▀ ▀░▀▀ ")
 print("")
-print("")
-print("выберите 1 что-бы добавить приложение")
-print("")
-print("выберите 2 что-бы запустить приложение")
-print("")
-print("выберите 3 что-бы удалить приложение")
-print("")
-print("выберите 4 что-бы зайти в список программ")
-print("")
+print("▄▀█ █▀█ █▀█ █░░ ▄▀█ █░█ █▄░█ █▀▀ █░█ █▀▀ █▀█")
+print("█▀█ █▀▀ █▀▀ █▄▄ █▀█ █▄█ █░▀█ █▄▄ █▀█ ██▄ █▀▄")
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("выберите 1 что-бы добавить приложение      |")
+print("                                           |")
+print("выберите 2 что-бы зайти в список программ  |")
+print("                                           |")
+print("выберите 3 что-бы запустить приложение     |")
+print("                                           |")
+print("выберите 4 что-бы удалить приложение       |")
+print("-"*43)
 
 
 
@@ -28,7 +27,8 @@ class app:
     def add_app(self):
         way_app.append(self.app_way)
         name_app.append(self.app_name)
-        print(self.app_name,self.app_way)
+        #print(self.app_name,self.app_way)
+        print("✅ приложение добавлено")
 
 def app_input():
       global app_input_way,app_input_name
@@ -45,24 +45,26 @@ while True:
         choose = 0
     
     if choose == 2:
-        print("введите название программы")
-        searh_programm = input()
         print(f"приложение:{name_app}")
         
     if choose == 3:
        
-       programm_name = input("ведите название программы")
+       programm_name = input("ведите название программы:")
        start=name_app.index(programm_name)
+       
        print(start)
+
        file_start = way_app[start]
        print(file_start)
        subprocess.Popen(file_start)
+       print("⚙️  приложение запускается")
 
     if choose == 4:
-        del_app = input("ведите название программы")
+        del_app = input("ведите название программы:")
         del_app_in = name_app.index(del_app)
         name_app.pop(del_app_in)
         way_app.pop(del_app_in)
+        print("🗑️  приложение удаленно")
 
     if choose == -1:
         break
